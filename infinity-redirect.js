48260323d1fee9868910050ef34a916b8b6c196b
546pvp <79662987+546pvp@users.noreply.github.com>
2022-04-07 18:17:54 +0200

    Update infinity-redirect.js

--- infinity-redirect.js
+++ infinity-redirect.js
@@ -3,3 +3,5 @@ let hostfinityred = location.hostname
 while( location.hostname = location.hostname) {
 location.redirect(location.hostname)
 }
+
+//NOTE: This file will work only if you upload it to your site
