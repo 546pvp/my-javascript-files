5b3ccd967ec57078492bc998a2de832514ca4931
546pvp <79662987+546pvp@users.noreply.github.com>
2022-04-08 13:42:52 +0200

    Update crash.js

--- crash.js
+++ crash.js
@@ -1,5 +1,6 @@
 const crashComposedRope = 1
-let = crashTexf // In the variable “Texf”  there is not mistake
+let crashTexf // In the variable “Texf”  there is not mistake
 while(crashComposedRope == 1) {
-    crashTexf += “Random things”
+    crashTexf += crashTexf
+    console.log(crashTexf)
 }
